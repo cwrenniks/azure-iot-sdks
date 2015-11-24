bd6641196232cfa7fd0a5ef5b180bc1e72b73367
Cameron Skinner <cwrenniks@gmail.com>
2015-11-24 11:49:20 -0800

    Structure of OSX support in place, now to implement the HTTP apis

---
+++ c/serializer/samples/simplesample_http/osx/simplesample_http/main.mm
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simplesample_http
+//
+//  Created by Cameron Skinner on 11/24/15.
+//  Copyright © 2015 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "simplesample_http.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        simplesample_http_run();
+    }
+    return 0;
+}
